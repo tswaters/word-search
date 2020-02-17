00bd90e38a78d8d0bb9d482a6274d38645ab353d
Tyler Waters <tyler.waters@gmail.com>
2020-02-16 19:21:37 -0800

    add populate routine

---
+++ test/populate.test.js
@@ -0,0 +1,31 @@
+import assert from 'assert'
+import { random } from '../lib/random'
+import { populate } from '../lib/grid'
+
+const sizes = {
+  '5x5': {
+    xmax: 4,
+    ymax: 4,
+    source: ['foo', 'bar', 'baz', 'qux'],
+    rnd: random(3)
+  }
+}
+
+describe('populate', () =>
+  Object.entries(sizes).forEach(([size, opts]) =>
+    it(size, () => {
+      const { placedWords, board } = populate(opts)
+      assert.deepEqual(
+        board.map(x => (x.letter ? x.letter : ' ')),
+        // prettier-ignore
+        [
+          ' ', ' ', 'f', 'o', 'o',
+          'q', 'b', 'b', 'a', 'r',
+          ' ', 'u', 'a', ' ', ' ',
+          ' ', ' ', 'x', 'z', ' ',
+          ' ', ' ', ' ', ' ', ' '
+        ]
+      )
+      assert.deepEqual(placedWords.sort(), opts.source.sort())
+    })
+  ))
