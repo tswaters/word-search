78bc3768d3420091da932291287f4b6072b7c4d0
Tyler Waters <tyler.waters@gmail.com>
2020-04-10 18:28:48 -0700

    Allow overlays to go left

--- components/Overlay.js
+++ components/Overlay.js
@@ -15,57 +15,20 @@ const Overlay = ({ range, className = '' }) => {
   const [startX, startY] = xy(xmax, startIndex)
   const [endX, endY] = xy(xmax, endIndex)
 
-  const deltaX = endX - startX
-  const deltaY = endY - startY
-  const widgetX = cellWidth * deltaX
-  const widgetY = cellHeight * deltaY
-  const widgetWidth = (widgetX ** 2 + widgetY ** 2) ** 0.5 // pothag, my man
-  let rotateDeg = Math.atan(widgetY / widgetX) / (Math.PI / 180)
-  if (endX < startX) {
-    console.log(
-      `delta: {${deltaX},${deltaY}}, widgetWidth ${widgetWidth}, rotateDeg ${rotateDeg.toFixed(
-        2
-      )}`
-    )
-  }
-
+  const widgetX = cellWidth * (endX - startX)
+  const widgetY = cellHeight * (endY - startY)
   const cssTop = cellHeight * startY
   const cssLeft = cellWidth * startX
-  let cssWidth = null
-  let cssHeight = null
-
-  // the widths/heights will be different based on the rotation
-  // at 90deg (vertical) the width becomes height and vice versa
-  switch (rotateDeg) {
-    case 0:
-      // horizontal
-      cssWidth = cellWidth + widgetWidth
-      cssHeight = cellHeight
-      break
-    case 90:
-      // vertical
-      cssWidth = cellHeight + widgetWidth
-      cssHeight = cellWidth
-      break
-    default:
-      // diagonal
-      cssWidth = Math.max(cellWidth, cellHeight) + widgetWidth
-      cssHeight = Math.min(cellWidth, cellHeight)
-  }
-
+  const inverse = endX < startX ? 180 : 0
+  const rotateDeg = Math.atan(widgetY / widgetX) / (Math.PI / 180) - inverse
+  const baseWidth = Math.max(cellWidth, cellHeight)
   const style = {
-    top: `${cssTop}px`,
-    left: `${cssLeft}px`,
-    height: `${cssHeight}px`,
-    width: `${cssWidth}px`,
+    top: `${cellHeight * startY}px`,
+    left: `${cellWidth * startX}px`
+    height: `${Math.min(cellWidth, cellHeight)}px`,
+    width: `${baseWidth + (widgetX ** 2 + widgetY ** 2) ** 0.5}px`,
     transform: `rotate(${rotateDeg.toFixed(2)}deg)`,
     transformOrigin: `${cellWidth / 2}px center`
-  }
-
-  if (rotateDeg !== 90 && rotateDeg !== 0) {
-    style.transform = `${style.transform} skewX(-10deg)`
-    // style.top = `-${cellHeight / 2}px`
-    // style.left = `-${cellWidth / 2}px`
   }
 
   return <div className={`${className} ${overlay}`} style={style} />
